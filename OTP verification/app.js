405658ccce4d0810061cb8af706aa407fb929537
shadan30 <37139445+shadan30@users.noreply.github.com>
2020-05-17 14:41:18 +0530

    Update app.js

--- OTP verification/app.js
+++ OTP verification/app.js
@@ -4,16 +4,16 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('/second.ejs');
+  res.render('/views/second.ejs');
   alert("Mobile number accepted");
 });
 
 app.get('/register', (req, res) => {
-  res.render('/first.ejs');
+  res.render('/views/first.ejs');
   alert("OTP Verified");
 });
 app.get('/final', (req, res) => {
-  res.render('/final.ejs');
+  res.render('/views/final.ejs');
 });
 
 
